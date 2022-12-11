3fe23c2551566b771bb8d356cca782392e0b915b
Aung-Thu-Zaw <sociallearning2022@gmail.com>
2022-12-11 23:25:19 +0630

    user authentication with laravel breeze

---
+++ public/assets/script.js
@@ -0,0 +1,28 @@
+const passwordInput1 = document.querySelector(".control-password-input1");
+const passwordInput2 = document.querySelector(".control-password-input2");
+const eyeIcon1 = document.querySelector(".eye-icon1");
+const eyeIcon2 = document.querySelector(".eye-icon2");
+
+eyeIcon1.addEventListener("click", () => {
+  if (passwordInput1.type == "password") {
+    passwordInput1.type = "text";
+    eyeIcon1.classList.remove("fa-eye");
+    eyeIcon1.classList.add("fa-eye-slash");
+  } else {
+    passwordInput1.type = "password";
+    eyeIcon1.classList.remove("fa-eye-slash");
+    eyeIcon1.classList.add("fa-eye");
+  }
+});
+
+eyeIcon2.addEventListener("click", () => {
+  if (passwordInput2.type == "password") {
+    passwordInput2.type = "text";
+    eyeIcon2.classList.remove("fa-eye");
+    eyeIcon2.classList.add("fa-eye-slash");
+  } else {
+    passwordInput2.type = "password";
+    eyeIcon2.classList.remove("fa-eye-slash");
+    eyeIcon2.classList.add("fa-eye");
+  }
+});
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,64 @@
+<x-guest-layout>
+    <h1 class="text-center text-primary mb-5">Login Here</h1>
+    <form action="{{ url('/login') }}" method="POST">
+        @csrf
+        @if (session()->has("status"))
+        <div class="alert alert-success">
+            <p class="text-center mb-0">{{ session("status") }}</p>
+        </div>
+        @endif
+        <div class="form-floating mb-3">
+            <input type="email" name="email" value="{{ old('email') }}" class="form-control" id="floatingEmail"
+                placeholder="name@example.com" required />
+            <label for="floatingEmail">Email address</label>
+
+            @error("email")
+            <p class="text-center text-danger">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="form-floating mb-3">
+            <input type="password" name="password" class="form-control control-password-input1" id="floatingPassword"
+                placeholder="Password" required />
+            <i class="fa-solid fa-eye eye-icon1"></i>
+            <label for="floatingPassword">Password</label>
+
+            @error("email")
+            <p class="text-center text-danger">{{ $message }}</p>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <input type="checkbox" name="remember" class="form-check-input me-2" />
+            <label for="remember">Remember me</label>
+        </div>
+        <div class="mb-3 d-grid">
+            <button type="submit" class="btn btn-primary btn-lg">Login</button>
+        </div>
+        <div class="mb-3">
+            <a href="{{ route('password.request') }}" class="text-primary">Forgot Password?</a>
+        </div>
+        <div class="mb-3">
+            <p class="text-center">
+                You don't have an account! Please
+                <a href="{{ route('register') }}">Register Here</a>
+            </p>
+        </div>
+        <div class="text-center p-1 border border-0 border-top border-bottom">
+            <span class="text-secondary rounded-circle"> Or: </span>
+        </div>
+        <div class="text-center p-2 d-flex flex-column align-items-center">
+            <p class="">Sign in with</p>
+            <div class="w-50 d-flex align-items center justify-content-around">
+                <div class="icon facebook">
+                    <i class="fa-brands fa-facebook"></i>
+                </div>
+                <div class="icon google">
+                    <i class="fa-brands fa-google"></i>
+                </div>
+                <div class="icon twitter">
+                    <i class="fa-brands fa-twitter"></i>
+                </div>
+            </div>
+        </div>
+    </form>
+</x-guest-layout>
